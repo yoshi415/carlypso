46fc962a0940f204a704da47f4a18b0ecdd9c336
Yoshio Varney <yoshio.varney@gmail.com>
2016-03-21 18:50:52 -0700

    initial deploy

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,14 @@
 import 'normalize.css';
 import angular from 'angular';
-import uiRouter from 'angular-ui-router'
+import uiRouter from 'angular-ui-router';
+import ngAnimate from 'angular-animate';
 import {appDirective} from './app.directive';
 import {api} from './components/api/api';
 import {listings} from './components/listings/listings';
 
 angular.module('app', [
   uiRouter,
+  ngAnimate,
   listings.name,
   api.name
 ])
